dad2bb38e194e685c787bf4126b6031adaf8f74b
mayank-gupta201 <mayankgoyal3005@gmail.com>
2026-04-08 23:48:33 +0530

    Fix package structure for OpenEnv push

--- server/__init__.py
+++
@@ -1,1 +0,0 @@
-# Server package (OpenEnv-compliant entry point shim).
--- ticketflow_env/server/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Server exports for TicketFlowEnv."""
-
-from ticketflow_env.server.app import app, create_app
-from ticketflow_env.server.environment import TicketFlowEnvironment
-
-__all__ = ["app", "create_app", "TicketFlowEnvironment"]
